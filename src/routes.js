a81134de0c6e9381340c985cb86970edbe2d2fd0
Matheus-souza-araujo <matheus1mrs@gmail.com>
2021-03-25 21:20:57 -0300

    Finalizado

--- src/routes.js
+++ src/routes.js
@@ -6,16 +6,43 @@ const axios = require('axios');
 routes.use(express.json());
 routes.use(cors());
 
-const tokeen = [];
+const tokeen = [{
+  token: 0,
+  tokenExpiration: 0
+}];
 const dados = []
 
-routes.post('/', async(req, res) => {
+
+function date (timestamp){
+  const date = new Date(timestamp)
+
+  //yyyy
+  const year = date.getUTCFullYear()
+  //mm
+  const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+  //dd
+  const day = `0${date.getUTCDate()}`.slice(-2)
+
+  const hours = date.getHours()
+
+  const minutes = date.getMinutes()
+
+  const seconds = date.getSeconds()
+  
+
+  return {
+  
+      iso: `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+  } 
+}
+routes.post('/geraToken', async(req, res) => {
   const { usuario, senha } = req.body
   const auth = { usuario, senha }
   const  { data }  = await axios.post('http://servicosflex.rpinfo.com.br:9000/v1.1/auth', auth)
   
   let { token, tokenExpiration } = data.response
 
+  
   const tokens = {
     token,
     tokenExpiration,
@@ -31,6 +58,14 @@ routes.post('/', async(req, res) => {
 }) 
 
 routes.get('/consulta', async (req, res) => {
+  const now = Date.now()
+  const nowFormat = date(now)
+
+
+  if(tokeen[0].tokenExpiration === 0 || nowFormat < tokeen[0].tokenExpiration){
+    return res.json({error: "Você não possui um token ou seu token expirou! Gere um novo"})
+  }
+
   const meuToken =  tokeen[0].token
   const { data } = await axios.get('http://servicosflex.rpinfo.com.br:9000/v2.0/produtounidade/listaprodutos/0/unidade/83402711000110?token=', {
     headers: {
